08e9b54f1333f6677c57d82266e1f9d316af8e44
Nater <natomato@users.noreply.github.com>
2022-10-29 15:44:58 +0200

    Add database.php config file back into git
    best to track it until a proper .env file is setup

--- server/database.php
+++ server/database.php
@@ -2,7 +2,7 @@
   date_default_timezone_set('America/New_York');
 
 	function getConnection() {
-		$dbh = new PDO("sqlite:/home/public/food-map/db/airtable.sqlite");
+		$dbh = new PDO("sqlite:/Users/natobyte/Programming/FoodParent2.0/db/airtable.sqlite");
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $dbh;
 	}
