7dfefec2e3dcff76b53f3be57f7543abea6d8898
Nater <natomato@users.noreply.github.com>
2023-05-18 18:58:27 -0400

    Improve db download script
    set flags to abort on any failed subtask
    fix string interpolation bug
    add missing steps to make the new db writeable

--- scripts/db_download.sh
+++ scripts/db_download.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
+set -eu
 
-# REQUIREMENTS
-# ---
-GITHUB_PAT='<a personal access token user creates to access concrete-jungle-org/cj-airtable>'
-# File exists /home/public/tree-map/db
+GITHUB_PAT=${A_TOKEN_YOU_CREATE_FOR_THIS_PURPOSE}
 
-curl -v \
-  -H 'Authorization: Bearer ' $GITHUB_PAT \
+echo curl -v \
+  -H "Authorization: Bearer $GITHUB_PAT" \
   -H 'Accept: application/vnd.github.v3.raw' \
   -o '/home/public/tree-map/db/airtable.sqlite'\
   -L 'https://api.github.com/repos/concrete-jungle-org/cj-airtable/contents/tree_parent.db'
 
+exec chgrp web /home/public/tree-map/db/airtable.sqlite
+exec chmod 664 /home/public/tree-map/db/airtable.sqlite
 exec sqlite3 /home/public/tree-map/db/airtable.sqlite < /home/public/tree-map/db/alter_food_col.sql
 exec sqlite3 /home/public/tree-map/db/airtable.sqlite < /home/public/tree-map/db/create_in_season_tbl.sql
