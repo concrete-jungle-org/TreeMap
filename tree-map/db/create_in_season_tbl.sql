5a7a8d38c06dc4555f4106ed9619e83cc0b12a3b
Nate Tate <natomato@users.noreply.github.com>
2023-05-18 09:14:06 -0400

    Update create_in_season_tbl.sql
    add protection if table not exist

--- tree-map/db/create_in_season_tbl.sql
+++ tree-map/db/create_in_season_tbl.sql
@@ -1,6 +1,8 @@
 --This table is a fixed view of data in the Donat table
 --It makes it easier to determine when each fruit is ripe
 --and ready to show on tree-map
+DROP TABLE IF EXISTS In_Season;
+
 CREATE TABLE In_Season (
   food_id TEXT NOT NULL,
   day_of_year INTEGER NOT NULL,
