9b629f65ae640045b3e46b07e0e26457a7279a51
Nater <natomato@users.noreply.github.com>
2023-05-23 16:20:27 -0400

    Change db name in script to match staging and prod

---
+++ scripts/db_copy.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+set -eu
+
+DB_PATH=/home/public/tree-map/db
+
+mv $DB_PATH/airtable.sqlite $DB_PATH/airtable_old.sqlite
+mv $DB_PATH/tree_parent.db $DB_PATH/airtable.sqlite
+chgrp web $DB_PATH/airtable.sqlite
+chmod 664 $DB_PATH/airtable.sqlite
+sqlite3 $DB_PATH/airtable.sqlite < $DB_PATH/alter_food_col.sql
+sqlite3 $DB_PATH/airtable.sqlite < $DB_PATH/create_in_season_tbl.sql
