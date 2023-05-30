399e7bd6bc34b961a7d10dbc928e71a25e3c1222
Nater <natomato@users.noreply.github.com>
2023-05-30 10:54:03 -0400

    Modify script to set group write permissions
    sqlite requires write permissions to parent dir
    for the web group user to be able to write to sqlite file

--- scripts/db_copy.sh
+++ scripts/db_copy.sh
@@ -5,6 +5,8 @@ DB_PATH=/home/public/tree-map/db
 
 mv $DB_PATH/airtable.sqlite $DB_PATH/airtable_old.sqlite
 mv $DB_PATH/tree_parent.db $DB_PATH/airtable.sqlite
+chmod g+w /home/public/tree-map
+chmod g+w /home/public/tree-map/db
 chgrp web $DB_PATH/airtable.sqlite
 chmod 664 $DB_PATH/airtable.sqlite
 sqlite3 $DB_PATH/airtable.sqlite < $DB_PATH/alter_food_col.sql
